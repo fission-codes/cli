bdf76baa31a25492ed6ab9d164d2543d4835ff33
Ben Church <ben.douglas.church@gmail.com>
2019-10-28 15:30:53 -0600

    Add Environment types

---
+++ library/Fission/CLI/Environment/Types.hs
@@ -0,0 +1,25 @@
+module Fission.CLI.Environment.Types (Environment (..)) where
+
+import RIO
+
+import Data.Aeson
+import           Servant.API
+
+import qualified Fission.IPFS.Types       as IPFS
+import           Fission.Internal.Orphanage.BasicAuthData ()
+
+data Environment = Environment
+  { userAuth :: BasicAuthData
+  , peers    :: NonEmpty IPFS.Peer
+  }
+
+instance ToJSON Environment where
+  toJSON Environment {..} = object
+    [ "user_auth" .= userAuth
+    , "peers"     .= peers
+    ]
+
+instance FromJSON Environment where
+  parseJSON = withObject "Environment" $ \obj ->
+    Environment <$> obj .: "userAuth"
+                <*> obj .: "peers"
