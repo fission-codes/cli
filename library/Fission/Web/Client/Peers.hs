cdba6584104078e3d0f81e46ff39579bab5df37c
Ben Church <ben.douglas.church@gmail.com>
2019-10-29 19:13:48 -0600

    Fill in documentation

--- library/Fission/Web/Client/Peers.hs
+++ library/Fission/Web/Client/Peers.hs
@@ -1,4 +1,4 @@
--- | BEN FILL ME IN
+-- | Servant client for retrieving peer data
 module Fission.Web.Client.Peers (get) where
 
 import RIO
@@ -9,9 +9,9 @@ import Servant.Client
 import qualified Fission.IPFS.Types    as IPFS
 import qualified Fission.Web.IPFS.Peer as Peer
 
--- | WHAT'S THIS FOR?
+-- | API path to the peers endpoints
 type API = "ipfs" :> "peers" :> Peer.API
 
--- | DESCRIBE ME!
+-- | Retrieve a list of peers from the fission api
 get :: ClientM [IPFS.Peer]
 get = client (Proxy :: Proxy API)
