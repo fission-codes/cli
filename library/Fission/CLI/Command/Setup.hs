8385f3b577ffe24151f97d5047e273ba6d8c66b2
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-02-10 16:46:49 -0700

    WIP

--- library/Fission/CLI/Command/Setup.hs
+++ library/Fission/CLI/Command/Setup.hs
@@ -46,54 +46,47 @@ setup ::
   => m ()
 setup =
   Key.exists >>= \case
-    True -> do 
+    True -> do
       Client.run User.Client.verify >>= \case
         Left err -> CLI.Error.put err
           "We don't recognize your key! Please contact Fission support or delete `~/.ssh/fission` and try again."
+
         Right (User.Username username) ->
-          CLI.Success.loggedInAs <| username
+          CLI.Success.loggedInAs username
 
     False ->
-      Env.Partial.findBasicAuth >>= \case
-        Nothing -> do
-          username <- Prompt.reaskNotEmpty' "Username: "
-          email <- Prompt.reaskNotEmpty' "Email: "
-          createAccount username email
-        Just auth -> 
-          upgradeAccount auth 
+      maybe createAccount upgradeAccount =<< Env.Partial.findBasicAuth
 
 createAccount ::
   ( MonadIO        m
   , MonadLogger    m
   , MonadWebClient m
   )
-  => Text
-  -> Text
+  => m ()
+createAccount = do
+  username <- User.Username <$> Prompt.reaskNotEmpty' "Username: "
+  email    <- User.Email <$> Prompt.reaskNotEmpty' "Email: "
+  register username email
+
+register ::
+  ( MonadIO        m
+  , MonadLogger    m
+  , MonadWebClient m
+  )
+  => User.Username
+  -> User.Email
   -> m ()
-createAccount username email = do
-  emailResp <- Client.run <| User.Client.getEmail <| User.Username username
+register username email =
+  User.Registration {..}
+    |> User.Client.register
+    |> Client.run
+    |> bind \case
+      Left err  -> do
+        CLI.Error.put err "It looks like that account already exists. Please pick another username or contact Fission support for account recovery."
+        createAccount
 
-  case emailResp of
-    Right (User.Email email') -> do
-      if email == email'
-      then do
-        -- @@TODO: recover flow
-        UTF8.putTextLn "😕 Looks like that account already exists. Please pick another username or contact Fission support for account recovery."
-        return ()
-      else do
-        UTF8.putTextLn "😕 That username's already taken. Try again?"
-        username' <- Prompt.reaskNotEmpty' "Username: "
-        createAccount username' email
-
-    Left err ->
-      case Client.is404 err of
-        False -> 
-          CLI.Error.put err "Could not reach Fission Server"
-        True -> do
-          -- If 404, user does not exist yet
-          createKey
-          UTF8.putText "📝 Registering your new account... "
-          register username email
+      Right _ok ->
+        CLI.Success.putOk "Registration successful!"
 
 upgradeAccount ::
   ( MonadIO        m
@@ -103,11 +96,12 @@ upgradeAccount ::
   => BasicAuthData
   -> m ()
 upgradeAccount auth = do
-  shouldUpgrade <- Prompt.reaskYN <| mconcat 
-                [ "Upgrade account \"" 
+  shouldUpgrade <- Prompt.reaskYN <| mconcat
+                [ "Upgrade account \""
                 , decodeUtf8Lenient (basicAuthUsername auth)
                 , "\"? (y/n) "
                 ]
+
   case shouldUpgrade of
     False -> return ()
     True -> do
@@ -127,24 +121,6 @@ createKey = do
   Key.forceCreate
   UTF8.putTextLn "done"
 
-register ::
-  ( MonadIO        m
-  , MonadLogger    m
-  , MonadWebClient m
-  )
-  => Text
-  -> Text
-  -> m ()
-register username email = do
-  User.Registration {..}
-    |> User.Client.register
-    |> Client.run
-    |> bind \case
-      Left err ->
-        CLI.Error.put err "Could not register account"
-      Right _ok -> 
-        CLI.Success.putOk "Registration successful!"
-
 updateDID ::
   ( MonadIO        m
   , MonadLogger    m
@@ -153,12 +129,12 @@ updateDID ::
   => BasicAuthData
   -> DID
   -> m ()
-updateDID auth did = 
+updateDID auth did =
   did
     |> User.Client.updateDID auth
     |> Client.run
     |> bind \case
-      Left err -> 
+      Left err ->
         CLI.Error.put err "Could not upgrade account"
       Right _ok -> do
         _ <- Env.Partial.deleteHomeAuth
--- library/Fission/Web/Client/User.hs
+++ library/Fission/Web/Client/User.hs
@@ -2,7 +2,6 @@ module Fission.Web.Client.User
   ( register
   , verify
   , updateDID
-  , getEmail
   ) where
 
 import Fission.Prelude
@@ -12,21 +11,17 @@ import Servant.Client
 
 import qualified Fission.User.Registration.Types as User
 import qualified Fission.User.Username.Types     as User
-import qualified Fission.User.Email.Types        as User
-import qualified Fission.User.DID.Types        as User
+import qualified Fission.User.DID.Types          as User
 
 import           Fission.Web.Client
-import qualified Fission.Web.User       as User
+import qualified Fission.Web.User   as User
 import           Fission.Web.Routes (UserPrefix)
 
 register  :: User.Registration -> ClientM ()
 register = registerClient <| Proxy @(UserPrefix :> User.RegisterRoute)
 
-verify    :: ClientM User.Username
+verify :: ClientM User.Username
 verify = sigClient' <| Proxy @(UserPrefix :> User.VerifyRoute)
 
 updateDID :: BasicAuthData -> User.DID -> ClientM User.DID
 updateDID = basicClient <| Proxy @(UserPrefix :> User.UpdateDIDRoute)
-
-getEmail  :: User.Username -> ClientM User.Email
-getEmail = client <| Proxy @(UserPrefix :> User.EmailRoute)
