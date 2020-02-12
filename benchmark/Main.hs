62c7e02ed50a8f4578aa3bd1feebad8e4c39f7e6
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-02-12 14:57:08 -0700

    Get bench to compiile

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -9,11 +9,11 @@ import qualified RIO.ByteString.Lazy   as Lazy
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "Textable.encode" encode
+  [ bgroup "Textable.encode" encode'
   ]
 
-encode :: [Benchmark]
-encode =
+encode' :: [Benchmark]
+encode' =
   [ bench "Strict ByteString" <| whnf UTF8.encode ("hello world" :: ByteString)
   , bench "Lazy ByteString" <| whnf UTF8.encode ("hello world" :: Lazy.ByteString)
   ]
