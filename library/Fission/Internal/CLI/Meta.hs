69ccbaa48e7ddc979aaf699e9b6e4a36c37940ae
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-02-12 17:06:56 -0700

    Reuse

--- library/Fission/Internal/CLI/Meta.hs
+++ library/Fission/Internal/CLI/Meta.hs
@@ -1,23 +1,10 @@
 module Fission.Internal.CLI.Meta
-  ( Package (..)
+  ( module Fission.Internal.Meta.Package
   , package
   ) where
 
-import Data.FileEmbed
-import Data.Yaml as Yaml
-
 import Fission.Prelude
-
-data Package = Package
-  { name    :: !(Maybe Text)
-  , version :: !(Maybe Text)
-  } deriving (Show, Eq)
-
-instance FromJSON Package where
-  parseJSON = withObject "Package" \obj -> do
-    name    <- obj .:? "name"
-    version <- obj .:? "version"
-    return Package {..}
+import Fission.Internal.Meta.Package hiding (package)
 
 package :: Maybe Package
 package =
